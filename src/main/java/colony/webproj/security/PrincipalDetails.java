abf9babe56893ac4818eb3c9200399c797f818e5
채승지 <cotmdwl1405@gmail.com>
2023-05-07 21:58:56 +0900

    fixed : password to getPassword()

--- src/main/java/colony/webproj/security/PrincipalDetails.java
+++ src/main/java/colony/webproj/security/PrincipalDetails.java
@@ -31,7 +31,7 @@ public class PrincipalDetails implements UserDetails {
 
     @Override
     public String getPassword() {
-        return this.password;
+        return this.getPassword();
     }
     //dfssdfsf
 
