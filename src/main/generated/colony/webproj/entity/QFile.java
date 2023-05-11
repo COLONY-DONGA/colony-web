7cc28c8049d77e0af8d84df3c7826c8f0677d86d
dgjinsu <rlawlstn3206@naver.com>
2023-05-11 21:48:10 +0900

    fixed: 스프링부트 3.0.x querydsl 설정 관련 오류 해결
    fixed: 스프링부트 3.0.x querydsl 설정 관련 오류 해결

---
+++ src/main/generated/colony/webproj/entity/QFile.java
@@ -0,0 +1,55 @@
+package colony.webproj.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QFile is a Querydsl query type for File
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QFile extends EntityPathBase<File> {
+
+    private static final long serialVersionUID = -29323242L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QFile file = new QFile("file");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath originFileName = createString("originFileName");
+
+    public final QPost post;
+
+    public final StringPath storeFileName = createString("storeFileName");
+
+    public QFile(String variable) {
+        this(File.class, forVariable(variable), INITS);
+    }
+
+    public QFile(Path<? extends File> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QFile(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QFile(PathMetadata metadata, PathInits inits) {
+        this(File.class, metadata, inits);
+    }
+
+    public QFile(Class<? extends File> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.post = inits.isInitialized("post") ? new QPost(forProperty("post"), inits.get("post")) : null;
+    }
+
+}
+
