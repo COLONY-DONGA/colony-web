ef5175420fbff8f2e7ec49f82fd1bbc19c36b4ec
dgjinsu <rlawlstn3206@naver.com>
2023-05-13 15:18:15 +0900

    feat: repo 생성

---
+++ src/main/java/colony/webproj/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package colony.webproj.repository;
+
+import colony.webproj.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
