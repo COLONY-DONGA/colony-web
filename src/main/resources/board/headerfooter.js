809fd9c10bce29be76bf36a51028af9c4b863e3c
Haning <100738360+Choiyuhyeon@users.noreply.github.com>
2023-05-16 22:12:57 +0900

    feat: 개발 환경 세팅

---
+++ src/main/resources/board/headerfooter.js
@@ -0,0 +1,16 @@
+// // headerFooter.js
+// // 헤더를 로드하고 페이지에 삽입
+// fetch('header.html')
+//   .then((response) => response.text())
+//   .then((data) => {
+//     document.getElementById('header-placeholder').innerHTML = data;
+//   });
+
+// // 푸터를 로드하고 페이지에 삽입
+// fetch('footer.html')
+//  xw .then((response) => response.text())
+//   .then((data) => {
+//     document.getElementById('footer-placeholder').innerHTML = data;
+//   });
+
+//헤더,푸터 css적용 안해서 주석처리 해놓았음
