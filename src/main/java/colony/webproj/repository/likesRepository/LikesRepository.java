b442b74da4f311079f6ad26386f53cf120a16fda
채승지 <cotmdwl1405@gmail.com>
2023-08-04 23:55:42 +0900

    feat: 좋아요 버그 수정 및 샘플 데이터 추가

--- src/main/java/colony/webproj/repository/likesRepository/LikesRepository.java
+++ src/main/java/colony/webproj/repository/likesRepository/LikesRepository.java
@@ -2,13 +2,16 @@ package colony.webproj.repository.likesRepository;
 
 import colony.webproj.entity.Likes;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface LikesRepository extends JpaRepository<Likes, Long> {
-    Optional<Likes> findByAnswerIdAndMemberId(Long answerId, String loginId);
+    @Query("select l from Likes l where l.member.loginId=:loginId and l.answer.id=:answerId")
+    Optional<Likes> findByAnswerIdAndMemberId(@Param("answerId")  Long answerId, @Param("loginId") String loginId);
 
 //    @Query("select count()")
 //    Long findHearNumByAnswerId(@Param("answerId") Long answerId);
