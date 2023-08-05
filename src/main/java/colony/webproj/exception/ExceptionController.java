d88fe88033678c1e5abd179a1e79e4e9dd7e523f
dgjinsu <rlawlstn3206@naver.com>
2023-08-05 18:29:37 +0900

    feat: 예외 핸들러 로그 추가

--- src/main/java/colony/webproj/exception/ExceptionController.java
+++ src/main/java/colony/webproj/exception/ExceptionController.java
@@ -1,22 +1,26 @@
 package colony.webproj.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Slf4j
 public class ExceptionController {
 
     @ExceptionHandler(CustomException.class)
     public String handleCustomException(CustomException e, Model model) {
         model.addAttribute("errorMessage", e.getErrorMessage());
         model.addAttribute("status", e.getStatus().toString());
+        log.info("핸들링한 에러 발생");
         return "errorPage";
     }
 
     @ExceptionHandler(Exception.class)
     public String handleCustomException(Exception e, Model model) {
         model.addAttribute("errorMessage", e.getMessage());
+        log.info("핸들링하지 않은 에러 발생");
         return "errorPage";
     }
 }
