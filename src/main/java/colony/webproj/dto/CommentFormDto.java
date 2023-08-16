06b6de2573af8e82f094661b5e6e9d268e870eb4
Yujin <qrt365@naver.com>
2023-08-16 16:45:28 +0900

    feat: 댓글 수정 기능 구현

--- src/main/java/colony/webproj/dto/CommentFormDto.java
+++ src/main/java/colony/webproj/dto/CommentFormDto.java
@@ -4,9 +4,11 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class CommentFormDto {
     private Long commentId; // update 에서 사용
