dbce4d4cc843935827fb6a5c6b2d5f3aef64b9b7
채승지 <cotmdwl1405@gmail.com>
2023-08-26 01:41:45 +0900

    feat: 현재 카테고리 이름도 가져오게 수정

--- src/main/java/colony/webproj/category/dto/CategoryDtoList.java
+++ src/main/java/colony/webproj/category/dto/CategoryDtoList.java
@@ -6,14 +6,25 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.Optional;
 
 @Getter@Setter
 public class CategoryDtoList {
     private List<CategoryDto> categoryDtoList;
     private Long presentCategoryId;
+    private String presentCategoryName;
 
     public CategoryDtoList(List<CategoryDto> categoryDtoList, Long presentCategoryId){
         this.categoryDtoList = categoryDtoList;
         this.presentCategoryId = presentCategoryId;
+        this.presentCategoryName=findCategoryNameById(presentCategoryId);
+    }
+
+    private String findCategoryNameById(Long id) {
+        Optional<CategoryDto> category = categoryDtoList.stream()
+                .filter(dto -> dto.getId().equals(id))
+                .findFirst();
+
+        return category.isPresent() ? category.get().getCategoryName() : null;
     }
 }
