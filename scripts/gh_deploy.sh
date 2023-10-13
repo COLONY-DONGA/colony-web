fa44dd82cf39dc6fd0ddfda95c3531360abf5872
dgjinsu <rlawlstn3206@naver.com>
2023-10-13 22:05:05 +0900

    docs: jar 파일 명 수정

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -40,4 +40,4 @@ echo "> 배포 종료 : $(date +%c)" >> $DEPLOY_LOG_PATH
 # nohup java -jar -Duser.timezone=Asia/Seoul web-proj-0.0.1-SNAPSHOT.jar >> application.log 2> deploy_err.log &
 
 # 모니터링
-# nohup java -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.port=8500 -Dcom.sun.management.jmxremote.rmi.port=8500 -Djava.rmi.server.hostname=3.37.206.141 -jar -Duser.timezone=Asia/Seoul tickerBell-0.0.1-SNAPSHOT.jar >> application.log 2> deploy_err.log &
+# nohup java -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.port=8500 -Dcom.sun.management.jmxremote.rmi.port=8500 -Djava.rmi.server.hostname=3.39.39.152 -jar -Duser.timezone=Asia/Seoul web-proj-0.0.1-SNAPSHOT.jar >> application.log 2> deploy_err.log &
